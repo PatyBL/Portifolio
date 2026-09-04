3ca42e1c285e9e036a062219c61f102b37664472
PatyBL <nikoras098@gmail.com>
2026-09-04 19:40:21 -0300

    0.1.1

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 #  Portifólio de Nicolas Gabriel Franco dos Santos
 
-Para um trbalho escolar sendo um recurso onde eu possa atualizar futuramente.
--- js/arquivo.js
+++ js/arquivo.js
@@ -1,16 +1,29 @@
-function sizeme(element) {
-    // Remove a classe "project-active" de todos os elementos com a classe "project"
+function sizeme(element){
     const projects = document.querySelectorAll('.project');
-    //verifica se estou clicando num item já clicado, se sim, remove a classe e retorna
-    if (element.classList.contains('project-active')) {
+
+    if(element.classList.contains('project-active')){
         element.classList.remove('project-active');
         return;
     }
-    // Tira a classe "project-active" de todos os elementos com a classe "project"
+
     projects.forEach(project => {
         project.classList.remove('project-active');
     });
-    // Adiciona a classe "project-active" ao elemento clicado
+
     element.classList.add('project-active');
 }
 
+function gallery(event, element){
+    event.stopPropagation();
+
+    if(element.classList.contains('img')){
+        element.classList.remove('img');
+        element.classList.add('img-a');
+        return;
+    }
+
+    if(element.classList.contains('img-a')){
+        element.classList.remove('img-a');
+        element.classList.add('img');
+    }
+}
