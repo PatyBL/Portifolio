9a9735fe0c8bd11df31589a0de324376a85e533d
PatyBL <nikoras098@gmail.com>
2026-09-04 21:00:58 -0300

    0.1.4

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,28 +1,49 @@
-//clicar e colocar o projeto em tamanho grande e clicar novamente para voltar ao tamanho normal
+// Clicar e colocar o projeto em tamanho grande e quando clicar novamente, voltar ao tamanho normal
 function sizeme(element){
     const projects = document.querySelectorAll('.project');
-
+    // Se o projeto já estiver maximizado, fecha
     if(element.classList.contains('project-active')){
         element.classList.remove('project-active');
+        // Força a galeria desse projeto a voltar para uma imagem
+        const galleryElement = element.querySelector('.img-a');
+
+        if(galleryElement){
+            galleryElement.classList.remove('img-a');
+            galleryElement.classList.add('img');
+        }
         return;
     }
-
+    // Fecha todos os outros projetos
     projects.forEach(project => {
         project.classList.remove('project-active');
-    });
+        // Força qualquer galeria aberta a fechar também
+        const galleryElement = project.querySelector('.img-a');
 
+        if(galleryElement){
+            galleryElement.classList.remove('img-a');
+            galleryElement.classList.add('img');
+        }
+    });
+    // Maximiza o projeto clicado
     element.classList.add('project-active');
 }
-//clicar e colocar todas as imagens em tamanho grande e clicar novamente para voltar ao tamanho normal
+
+// Clicar na imagem e abrir/fechar a galeria com a verificação de active já
 function gallery(event, element){
     event.stopPropagation();
+    // Impede a galeria de abrir caso o projeto não esteja maximizado sendo a unica verifaicação de active
+    const project = element.closest('.project');
 
+    if(!project.classList.contains('project-active')){
+        return;
+    }
+    // Abre a galeria
     if(element.classList.contains('img')){
         element.classList.remove('img');
         element.classList.add('img-a');
         return;
     }
-
+    // Fecha a galeria
     if(element.classList.contains('img-a')){
         element.classList.remove('img-a');
         element.classList.add('img');
